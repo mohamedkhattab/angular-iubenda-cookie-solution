9e6c95f5f14bbdac61ec5f161b2ab495e0647e23
Mohamed Khattab <muhammadkhattab292@outlook.com>
2021-03-19 11:19:55 +0200

    update deploy and CS CDN link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
     window["_iub"] = this.iubendaCookieSolution._iub;
     let script = document.createElement("script");
     script.src =
-      "http://localhost:3012//cookie_solution/iubenda_cs/core.js";
+      "http://cdn.iubenda.com/cookie_solution/iubenda_cs/core-d053bf0c05bb347ae5bf169b877657a0.js";
     document.head.appendChild(script);
   }
 }
