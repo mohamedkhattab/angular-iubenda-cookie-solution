4213a872ffb8c025b762bd59bef96cad28169070
Mohamed Khattab <muhammadkhattab292@outlook.com>
2021-03-19 11:31:13 +0200

    use https for CS CDN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
     window["_iub"] = this.iubendaCookieSolution._iub;
     let script = document.createElement("script");
     script.src =
-      "http://cdn.iubenda.com/cookie_solution/iubenda_cs/core-d053bf0c05bb347ae5bf169b877657a0.js";
+      "https://cdn.iubenda.com/cookie_solution/iubenda_cs/core-d053bf0c05bb347ae5bf169b877657a0.js";
     document.head.appendChild(script);
   }
 }
